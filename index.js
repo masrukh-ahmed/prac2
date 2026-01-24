3e1b4969b0796a7a73e4c6bda862de8f1c442d56
Masrukh Ahmed <masrukhahmed502@gmail.com>
2026-01-24 13:17:58 +0530

    Express backend, routing and template setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+let port = 3000;
+const path = require("path");
+
+app.set("view engine", "ejs"); //setting view engine as ejs
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("pricing");
+});
+
+app.listen(port, () => {
+  //for server startup
+  console.log(`Listening on port ${port}.`);
+});
